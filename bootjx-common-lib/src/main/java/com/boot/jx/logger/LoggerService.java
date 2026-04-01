a4a94aed1a742f05b4a885cabbac3f62c965f7b3
Lalit Tanwar <lalit.tanwar07@gmail.com>
2026-04-01 18:31:58 +0530

    Added Timer

--- bootjx-common-lib/src/main/java/com/boot/jx/logger/LoggerService.java
+++ bootjx-common-lib/src/main/java/com/boot/jx/logger/LoggerService.java
@@ -1,5 +1,8 @@
 package com.boot.jx.logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +10,49 @@ public class LoggerService {
 
 	public static Logger getLogger(Class<?> clazz) {
 		return LoggerFactory.getLogger(clazz);
+	}
+
+	private static class LogEntry {
+		String label;
+		long duration;
+
+		LogEntry(String label, long duration) {
+			this.label = label;
+			this.duration = duration;
+		}
+	}
+
+	public static class LogTimer {
+
+		private long lastTime;
+		private final List<LogEntry> logs = new ArrayList<>();
+
+		public LogTimer() {
+			this.lastTime = System.currentTimeMillis();
+		}
+
+		public void log(String label) {
+			long now = System.currentTimeMillis();
+			long diff = now - lastTime;
+
+			logs.add(new LogEntry(label, diff));
+			lastTime = now;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+
+			for (LogEntry entry : logs) {
+				sb.append(entry.label).append(" : ").append(entry.duration).append(" ms\n");
+			}
+
+			return sb.toString();
+		}
+	}
+
+	public static LogTimer getTimer() {
+		return new LogTimer();
 	}
 
 }
