608bd52043fd0ac3161f591c6f8b4d0d2e9829f4
Lalit Tanwar <lalit.tanwar07@gmail.com>
2026-06-08 12:06:56 +0530

    Email valid

--- bootjx-common-lib/src/main/java/com/boot/jx/validation/EmailValidator.java
+++ bootjx-common-lib/src/main/java/com/boot/jx/validation/EmailValidator.java
@@ -10,23 +10,27 @@ import com.boot.utils.ArgUtil;
 
 public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
 
-    public static final Pattern P1 = Pattern
-	    .compile("^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}");
+	public static final Pattern P1 = Pattern
+			.compile("^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}");
 
-    @Override
-    public void initialize(ValidEmail constraintAnnotation) {
+	@Override
+	public void initialize(ValidEmail constraintAnnotation) {
 
-    }
-
-    @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-	if (!ArgUtil.is(value)) {
-	    return false;
 	}
-	if (false || P1.matcher(value).matches()) {
-	    return true;
+
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		if (!ArgUtil.is(value)) {
+			return false;
+		}
+		if (false || P1.matcher(value).matches()) {
+			return true;
+		}
+		return false;
 	}
-	return false;
-    }
+
+	public boolean isValid(String value) {
+		return this.isValid(value, null);
+	}
 
 }
